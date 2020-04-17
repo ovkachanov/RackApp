04c318574b0fa3ae74f65a4ec3bc6adb6c9f5961
Oleg Kachanov <ovkachanov1988@gmail.com>
2020-04-17 18:37:02 +0300

    fix remarks

--- app.rb
+++ app.rb
@@ -1,36 +1,43 @@
 require 'rack'
 require 'rack/response'
+require_relative 'time_formatter'
 
 class App
   def call(env)
-    date = DateTime.now
-    @formats_time = { 'year' => date.year, 'month' => date.month, 'day' => date.day, 'hour' => date.hour,
-                      'minute' => date.minute, 'second' => date.second }
     @request = Rack::Request.new(env)
+    @timestring = TimeFormatter.new(@request.params['format'])
     logic
   end
 
   private
 
-  def params_split
-    @request.params['format'].split(',')
-  end
-
-  def valid_params
-    params_split.map { |format| @formats_time[format] }.compact.join('-')
-  end
-
-  def invalid_params
-    params_split.reject { |format| @formats_time[format] }
-  end
-
   def logic
     if @request.path != '/time'
-      [404, { 'Content-Type' => 'text/plain' }, ["Error request!"]]
-    elsif params_split.all? { |format| @formats_time.key?(format) }
-      [200, { 'Content-Type' => 'text/plain' }, [valid_params]]
+      not_found
+    elsif @timestring.valid_params?
+      correct_answer(@timestring.formatted_time)
     else
-      [400, { 'Content-Type' => 'text/plain' }, ["Unknown time format #{invalid_params}"]]
+      bad_request("Unknow time format: #{timestring.invalid_params}")
     end
+  end
+
+  def response(status:, headers: content_type, body: nil)
+    [status, headers, ["#{body}\n"]]
+  end
+
+  def content_type
+    { 'Content-Type' => 'text/plain' }
+  end
+
+  def not_found
+    response(status: 404, body: "Not Found")
+  end
+
+  def correct_answer(body)
+    response(status: 200, body: body)
+  end
+
+  def bad_request(body)
+    response(status: 400, body: body)
   end
 end
